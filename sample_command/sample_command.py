0abe26b73b4090d636a32a62dcda7b9c8d5c8e61
Yutaka-A <bestradlovur@gmail.com>
2020-07-22 17:55:12 +0900

    changed args place

--- sample_command/sample_command.py
+++ sample_command/sample_command.py
@@ -1,8 +1,8 @@
-def main(a, b):
+def main():
+    a = [1, 2, 3, 4, 5]
+    b = [2, 3, 4, 5, 6]
     x = set(a) & set(b)
     print(list(x))
 
 if __name__ == '__main__':
-    a = [1, 2, 3, 4, 5]
-    b = [2, 3, 4, 5, 6]
     main()
